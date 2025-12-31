e6870ebf8faf1078443e86863ae6ce68a329b157
Haseeb-km07 <haseebabdham2903@gmail.com>
2025-12-31 14:49:53 +0530

    Update and rename main.py to program 18/main.py

--- program 18/main.py
+++ program 18/main.py
@@ -1,4 +1,4 @@
-from graphics.rectanle import area, perimeter
+from graphics.rectangle import area, perimeter
 from graphics.circle import area as c_area, perimeter as c_peri
 from graphics.ThreeD_graphics.coboid import surface_area, perimeter as cub_peri
 from graphics.ThreeD_graphics.sphere import surface_area as s_area, perimeter as s_peri
